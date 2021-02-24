4e6e08a5ccbf58eb65383695d4501c2001a772c9
pmo48 <pmoriarty100@gmail.com>
2021-02-23 19:15:43 -0800

    navbar and search box

---
+++ employee-directory/src/components/SearchResults/index.js
@@ -0,0 +1,32 @@
+// import React, { Component } from "react";
+// import API from "../utils/API";
+// import Container from "../components/Container";
+// import SearchForm from "../components/SearchForm";
+// import SearchResults from "../components/SearchResults";
+// // import Alert from "../components/Alert";
+
+// class SearchResults extends Component {
+//   state = {
+//     employees: [],
+//   };
+
+//   // When the component mounts, get a list of all available base breeds and update this.state.breeds
+//   componentDidMount() {
+//     API.getEmployees()
+//       .then(res => this.setState({ employees: res.data.message }))
+//       .catch(err => console.log(err));
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//           <h1 className="text-center">Search By Breed!</h1>
+      
+//           <p></p> 
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchResults;
+
---
+++ employee-directory/src/utils/ArticleContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArticleContext = React.createContext({
+  name: "",
+});
+
+export default ArticleContext;
